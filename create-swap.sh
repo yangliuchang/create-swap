5660e39c6e5cce5ab9aceed5597b44adf68259dc
yanglc <65783234+ylc429@users.noreply.github.com>
2021-06-21 04:19:17 +0800

    Update create-swap.sh

--- create-swap.sh
+++ create-swap.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
+RED_COLOR="\033[0;31m"
+NO_COLOR="\033[0m"
+GREEN="\033[32m\033[01m"
+BLUE="\033[0;36m"
+FUCHSIA="\033[0;35m"
+echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
+source ~/.bashrc
+sleep 3
 echo "Creat-SWAP by yanglc"
 echo "本脚本仅在ubuntu下进行过测试"
 get_char()
