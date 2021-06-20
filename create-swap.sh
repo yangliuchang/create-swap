e6d7921b551f7f53dd60bfef56d23baf11ee82ef
yanglc <65783234+ylc429@users.noreply.github.com>
2021-06-21 05:22:02 +0800

    Update create-swap.sh

--- create-swap.sh
+++ create-swap.sh
@@ -4,11 +4,9 @@ NO_COLOR="\033[0m"
 GREEN="\033[32m\033[01m"
 BLUE="\033[0;36m"
 FUCHSIA="\033[0;35m"
-echo "export PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:$PATH" >> ~/.bashrc
-source ~/.bashrc
-sleep 3
-echo "Creat-SWAP by yanglc"
-echo "本脚本仅在ubuntu下进行过测试"
+echo "
+      ${GREEN} Creat-SWAP by yanglc
+      ${GREEN} 本脚本仅在Debian系系统下进行过测试"
 get_char()
 {
     SAVEDSTTY=`stty -g`
@@ -19,20 +17,17 @@ get_char()
     stty echo
     stty $SAVEDSTTY
 }
-echo "按任意键添加SWAP分区："
-echo "使用CTRL+C退出脚本。。。"
-echo "Press any key to continue："
-echo " CTRL+C break command bash..."#CTRL+C EXIT
+echo " ${GREEN} 按任意键添加1G大小的SWAP分区："
 char=`get_char`
-echo "Please set the partition size you want："
-read -p "请输入要添加的大小:" size
-sleep 1
-echo "###########开始添加SWAP分区##########"
-dd if=/dev/zero of=/mnt/swap bs=1M count='$size'
+echo " ${GREEN} ###########开始添加SWAP分区##########"
+dd if=/dev/zero of=/mnt/swap bs=1M count=1024
 echo -e
-echo "###########设置交换分区文件##########"
+echo " ${GREEN} ###########设置交换分区文件##########"
 mkswap /mnt/swap
 echo -e
-echo "###########设置开机自启动############"
+echo " ${GREEN} ###########启动SWAP分区中...#########"
+swapon /mnt/swap
+echo -e
+echo " ${GREEN} ###########设置开机自启动############"
 echo '/mnt/swap swap swap defaults 0 0' >> /etc/fstab
-echo "All done！Thanks for using this shell script"
+echo " ${GREEN} All done！Thanks for using this shell script"
