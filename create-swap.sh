722ac296f304cc7e5404f3853d6a70b518875a87
yanglc <65783234+ylc429@users.noreply.github.com>
2021-06-21 18:49:27 +0800

    Update create-swap.sh

--- create-swap.sh
+++ create-swap.sh
@@ -13,10 +13,10 @@ get_char()
     stty echo
     stty $SAVEDSTTY
 }
-echo "按任意键添加1G大小的SWAP分区："
+echo "按任意键添加2G大小的SWAP分区："
 char=`get_char`
 echo "###########开始添加SWAP分区##########"
-dd if=/dev/zero of=/mnt/swap bs=1M count=1024
+dd if=/dev/zero of=/mnt/swap bs=1M count=2048
 echo -e
 echo " ###########设置交换分区文件##########"
 mkswap /mnt/swap
